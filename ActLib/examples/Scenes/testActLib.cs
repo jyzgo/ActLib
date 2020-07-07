17a8650ecfb73518304ca4e357e5f3989ed496cb
jyzgo <jyzgo@163.com>
2020-07-07 22:52:44 +0800

    add examples

---
+++ ActLib/examples/Scenes/testActLib.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ActLib
+{
+    public class testActLib : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
