65cbfe42e11a3d1bbb744fa3c14bc3e3c8ff8ea4
Ethan Jiang <jyzgo0125@gmail.com>
2020-07-22 19:08:44 +0800

    remove debug

--- ActLib/actLib/ActUIAnchorPositionChangeTo.cs
+++ ActLib/actLib/ActUIAnchorPositionChangeTo.cs
@@ -9,7 +9,6 @@ namespace ActLib
         public ActUIAnchorPositionChangeTo(float duration, Vector2 position)
         {
             _duration = duration;
-            Debug.Log("PP " + position);
             _endPosition = position;
         }
 
@@ -26,11 +25,9 @@ namespace ActLib
         {
             float elapsedTime = 0;
             Vector2 startingPos = _trans.anchoredPosition;
-            Debug.Log("name  " + _trans.name + "hih " + startingPos + " end " + _endPosition);
 
             while (elapsedTime < _duration)
             {
-                Debug.Log("inini " + elapsedTime);
                 _trans.anchoredPosition= Vector2.Lerp(startingPos, _endPosition, (elapsedTime / _duration));
                 elapsedTime += Time.deltaTime;
                 yield return new WaitForEndOfFrame();
